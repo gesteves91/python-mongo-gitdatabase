1ebda53a1e68b0d6865a40c93450b032eccb21ce
gesteves91 <esteves.gean@gmail.com>
2018-12-18 22:46:20 -0200

    add to the server this file that is going to be executed on kunumi

--- commits.py
+++ commits.py
@@ -2,12 +2,12 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
-mycol = mydb["commits.committer"]
+mycol = mydb["commits"]
 
 #myquery = { "_id": "52343e2ebd3543bb7f000002" }
 
 #mydoc = mycol.find(myquery)
-mydoc = mycol.distinct("id")
+mydoc = mycol.distinct("committer.id")
 
 for x in mydoc:
   print(x)
