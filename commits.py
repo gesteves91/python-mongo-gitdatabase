d6b13995a1c963bd034375366f4728920039f6a7
gesteves91 <esteves.gean@gmail.com>
2018-12-18 22:36:59 -0200

    try to access commits instead of repos

--- commits.py
+++ commits.py
@@ -2,7 +2,7 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
-mycol = mydb["repos"]
+mycol = mydb["commits"]
 
 #myquery = { "_id": "52343e2ebd3543bb7f000002" }
 
