7c8f87464e027c0f34f66cc61525f2c856db839c
gesteves91 <esteves.gean@gmail.com>
2018-12-19 00:31:31 -0200

    add the comment file

--- comments.py
+++ comments.py
@@ -4,9 +4,9 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
 mycol = mydb["commits_comments"]
 
-myquery = { "commit_id": "cba2407f4a86c0a040fe0473615f9987ab108b9c" }
+mydoc = mycol.distinct("commit_id")
 
-mydoc = mycol.find(myquery)
+#mydoc = mycol.find(myquery)
 
 for x in mydoc:
   print(x)
