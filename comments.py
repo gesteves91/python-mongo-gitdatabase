9c76576cdc85fb416fc1dd466f59f281181fdec3
gesteves91 <esteves.gean@gmail.com>
2018-12-19 09:33:15 -0200

    linter in the code

--- comments.py
+++ comments.py
@@ -4,9 +4,8 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
 mycol = mydb["commit_comments"]
 
+# Get all the unique commit ids.
 mydoc = mycol.distinct("commit_id")
-
-#mydoc = mycol.find(myquery)
 
 for x in mydoc:
   print(x)
