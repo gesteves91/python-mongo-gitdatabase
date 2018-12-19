70cf63d5be70f1f9c7a92e1e0dcba65aad4c6ebc
gesteves91 <esteves.gean@gmail.com>
2018-12-19 00:37:18 -0200

    add the comment file

--- commits.py
+++ commits.py
@@ -4,14 +4,14 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
 mycol = mydb["commits"]
 
-#myquery = { "_id": "52343e2ebd3543bb7f000002" }
+myquery = { "sha": "fffcc4e85c3eae0b976f2f081c23bab21fd289d9" }
 
-#mydoc = mycol.find(myquery)
+mydoc = mycol.find(myquery)
 #mydoc = mycol.distinct("committer.id")
 #cursor = mycol.find({})
 
-x = mycol.find_one()
-print(x)
+#x = mycol.find_one()
+#print(x)
 
-#for x in mydoc:
-#  print(x)
+for x in mydoc:
+  print(x)
