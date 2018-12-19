2d3355766cd6f1b9b8d7ade8308ba352584aff11
gesteves91 <esteves.gean@gmail.com>
2018-12-18 23:07:03 -0200

    add the comment file

--- commits.py
+++ commits.py
@@ -7,7 +7,7 @@ mycol = mydb["commits"]
 #myquery = { "_id": "52343e2ebd3543bb7f000002" }
 
 #mydoc = mycol.find(myquery)
-mydoc = mycol.distinct("committer.id")
+mydoc = mycol.distinct("sha")
 
 for x in mydoc:
   print(x)
