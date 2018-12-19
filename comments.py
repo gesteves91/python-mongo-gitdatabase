36d9d79521c92127d294fc8dbdd90ad7eb891492
gesteves91 <esteves.gean@gmail.com>
2018-12-19 16:24:18 -0200

    add the csv file writer

--- comments.py
+++ comments.py
@@ -4,8 +4,16 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["msr14"]
 mycol = mydb["commit_comments"]
 
+count = 0
+
+myquery = { "commit_id": 1 }
+
 # Get all the unique commit ids.
-mydoc = mycol.distinct("commit_id")
+#mydoc = mycol.distinct("commit_id")
+mydoc = mycol.find({}, myquery)
 
 for x in mydoc:
-  print(x)
+  #print(x)
+  count = count + 1
+
+print(count)
