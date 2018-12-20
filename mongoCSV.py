706bf068b87804fa5f88913d9361c78a4339ca81
Geanderson Esteves <esteves.gean@gmail.com>
2018-12-20 15:04:15 -0200

    linting the mongo file

--- mongoCSV.py
+++ mongoCSV.py
@@ -12,14 +12,10 @@ with open('stack.csv', 'w') as outfile:
     fields = ['id', 'commit_id', 'body']
     write = csv.DictWriter(outfile, fieldnames=fields)
     write.writeheader()
-    for commits in cursor:  # Here we are using 'cursor' as an iterator
-        id = commits['id']
-        sha = commits['commit_id']
-        body = commits['body']
-        #for answer_record in answers_record['answers']:
+    for commits in cursor:  
         flattened_record = {
-            'id': id,
-            'commit_id': sha,
-            'body': body,
+            'id': commits['id'],
+            'commit_id': commits['commit_id'],
+            'body': commits['body'],
         }
         write.writerow(flattened_record)
