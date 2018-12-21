ab949e7bd37746658dfa2178c65222890519857c
gesteves91 <esteves.gean@gmail.com>
2018-12-21 00:20:12 -0200

    a working verion of the stats

--- stats.py
+++ stats.py
@@ -6,20 +6,21 @@ mydb = myclient["msr14"]
 mycol = mydb["commits"]
 
 cursor = mycol.find(
-    {}, {'_id': 1, 'stats.total': 1, 'stats.additions': 1, 'stats.deletions': 1})
+    {}, {'stats.deletions': 1, 'stats.additions': 1, 'stats.total': 1})
 
 with open('commits.csv', 'w') as outfile:
-    fields = ['id', 'stats.total', 'stats.additions', 'stats.deletions']
+    fields = ['stats.deletions', 'stats.additions', 'stats.total']
     write = csv.DictWriter(outfile, fieldnames=fields)
     write.writeheader()
     for commits in cursor:
-        id = commits['_id']
-        for stats in commits['stats']:
-            print(type(stats))
-            flattened_record = {
-                '_id': id,
-                'stats.total': stats['total'],
-                'stats.additions': stats['additions'],
-                'stats.deletions': stats['deletions']
-            }
-            write.writerow(flattened_record)
+        if 'stats' in commits:
+            stats = commits['stats']
+            print(stats)
+            for key in stats:
+                flattened_record = {
+                'stats.deletions': stats[key],
+                'stats.additions': stats[key],
+                'stats.total': stats[key]
+                }
+                write.writerow(flattened_record)
+    print("Writing complete!!!")
